3c498ec814e07a16139ad27a03db0e4b8ff440fa
Max Stein <maxwell.stein@gmail.com>
2018-10-24 13:57:26 -0700

    location select worldwide

--- php/geocode.php
+++ php/geocode.php
@@ -15,7 +15,9 @@ if(isset($_GET['loc']) && $_GET['loc']!=""){
 	*/
 	
 	//	$json = file_get_contents('https://maps.googleapis.com/maps/api/place/autocomplete/json?input='.$loc.'&types=geocode&components=country:es&key=AIzaSyD4I4HZ25lHy9WftOj4x3fEKCoEGYmJHgk');
-	$json = file_get_contents('https://maps.googleapis.com/maps/api/place/autocomplete/json?input='.$loc.'&types=geocode&components=country:es&key=AIzaSyD4I4HZ25lHy9WftOj4x3fEKCoEGYmJHgk');
+	//$json = file_get_contents('https://maps.googleapis.com/maps/api/place/autocomplete/json?input='.$loc.'&types=geocode&components=country:es&key=AIzaSyD4I4HZ25lHy9WftOj4x3fEKCoEGYmJHgk');
+	$json = file_get_contents('https://maps.googleapis.com/maps/api/place/autocomplete/json?input='.$loc.'&types=geocode&components&key=AIzaSyD4I4HZ25lHy9WftOj4x3fEKCoEGYmJHgk');
+	
 	$data = json_decode($json);
 	//print_r($data);
 
